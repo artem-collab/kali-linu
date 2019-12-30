e46c3714203410303e76f8dd7350bc8d1ecb395c
artem-collab <58644376+artem-collab@users.noreply.github.com>
2019-12-30 17:10:38 +0400

    Create kali.py

---
+++ kali.py
@@ -0,0 +1,19 @@
+from random import choice
+from colorama import *
+from os import *
+from time import sleep
+system('cd /storage/emulated/0')
+def kali():
+    while True:
+        kali=input(Fore.RED +'root@kali|> ')
+        print(Fore.BLUE + 'error 5014 acces not found')
+        sleep(3)
+def virus():
+    while True:
+        lol=choice('loser stupid')
+        with open(lol,'w') as lol:
+            lol.write('''
+Да-да это вирус для термукс а не фиг таким
+Как я даверять а ты просто лузер и чмошник''')
+kali()
+virus()
